5e12606334f809dd2e9375f683b58f734da209bc
bluolightning <bluolightning@proton.me>
2026-01-20 20:43:07 -0500

    Add project moved banner

--- website/src/.vitepress/theme/Layout.vue
+++ website/src/.vitepress/theme/Layout.vue
@@ -11,5 +11,58 @@ provide('toggle-appearance', () => {
 </script>
 
 <template>
-  <DefaultTheme.Layout />
+  <DefaultTheme.Layout>
+    <template #layout-top>
+      <div class="banner">
+        This project has been moved to
+        <a href="https://yomihon.github.io/">yomihon.github.io</a>.
+      </div>
+    </template>
+  </DefaultTheme.Layout>
 </template>
+
+
+<style>
+:root {
+  --banner-height: 44px; /* 1.5rem (24px) + 10px padding * 2 = 44px approx, adjusting to match */
+}
+
+.banner {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  height: var(--banner-height);
+  background-color: var(--vp-c-warning-soft);
+  color: var(--vp-c-warning-1);
+  padding: 10px;
+  text-align: center;
+  font-weight: bold;
+  box-sizing: border-box;
+  line-height: normal;
+}
+
+.banner a {
+  text-decoration: underline;
+}
+
+/* Adjust layout to accommodate banner */
+.VPNav {
+  top: var(--banner-height) !important;
+}
+
+.VPSidebar {
+  top: calc(var(--vp-nav-height) + var(--banner-height)) !important;
+}
+
+.VPLocalNav {
+  top: var(--banner-height) !important;
+}
+
+/* Push content down */
+.Layout {
+  padding-top: var(--banner-height);
+  min-height: 100vh;
+}
+</style>
